a3dab4a517a46d944633a44aac4a1ca42753c997
Dmitry <mail@ya.ru>
2020-04-30 03:06:45 +0300

    Update set_clantag_ffi.lua

--- set_clantag_ffi.lua
+++ set_clantag_ffi.lua
@@ -5,4 +5,4 @@ ffi.cdef[[
 local fn_change_clantag = Utils.PatternScan("engine.dll", "53 56 57 8B DA 8B F9 FF 15")
 print(tostring(fn_change_clantag))
 local set_clantag = ffi.cast("clantag_t", fn_change_clantag)
-set_clantag("asd", "asd")
+set_clantag("spirthack.me", "spirthack.me")
